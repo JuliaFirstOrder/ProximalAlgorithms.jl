2df72eb3a2a2699f602425914dc72881d7157228
Puya Latafat <p.latafat@gmail.com>
2018-01-14 01:32:15 +0100

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,11 +8,11 @@ using Base.Test
 end
 
 @testset "Algorithms" begin
-    # include("test_template.jl")
-    # include("test_lasso_small.jl")
-    # include("test_lasso_small_split_x.jl")
-    # include("test_lasso_small_split_f.jl")
-    # include("test_l1logreg_small.jl")
+    include("test_template.jl")
+    include("test_lasso_small.jl")
+    include("test_lasso_small_split_x.jl")
+    include("test_lasso_small_split_f.jl")
+    include("test_l1logreg_small.jl")
     include("test_afba.jl")
 end
 end
