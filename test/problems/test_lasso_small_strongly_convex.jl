69b2fcad42a5f4a7e962c6fc345b5c2461c12c09
William Kong <wwkong92@gmail.com>
2021-09-23 22:46:56 -0400

    Requested changes from the PR.

--- test/problems/test_lasso_small_strongly_convex.jl
+++ test/problems/test_lasso_small_strongly_convex.jl
@@ -9,19 +9,19 @@ using ProximalAlgorithms
 
     Random.seed!(777)
     dim = 5
-    μf = T(1.0)
-    Lf = T(10.0)
+    μf = T(1)
+    Lf = T(10)
 
     x_star = convert(Vector{T}, 1.5 * rand(T, dim) .- 0.5)
 
-    lam = convert(T, (μf + Lf) / 2.0)
+    lam = (μf + Lf) / 2
     @test typeof(lam) == T
 
-    D = Diagonal(convert(Vector{T}, sqrt(μf) .+ (sqrt(Lf) - sqrt(μf)) * rand(T, dim)))
+    D = Diagonal(sqrt(μf) .+ (sqrt(Lf) - sqrt(μf)) * rand(T, dim))
     D[1] = sqrt(μf)
     D[end] = sqrt(Lf)
     Q = qr(rand(T, (dim, dim))).Q
-    A = convert(Matrix{T}, Q * D * Q')
+    A = Q * D * Q'
     b = A * x_star + lam * inv(A') * sign.(x_star)
 
     f = LeastSquares(A, b)
@@ -36,7 +36,7 @@ using ProximalAlgorithms
         x0 =  A \ b
         x0_backup = copy(x0)
         solver = ProximalAlgorithms.SFISTA(tol = TOL)
-        y, it = solver(x0, f = f, h = h, Lf = Lf)
+        y, it = solver(x0, f = f, h = h, Lf = Lf, μf = μf)
         @test eltype(y) == T
         @test norm(y - x_star) <= TOL
         @test it < 200
