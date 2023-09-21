037b0a90c1329c3d0fe38ba3641693a17b9c8e63
Lorenzo Stella <lorenzostella@gmail.com>
2023-09-21 23:53:54 +0200

    Fix piracy

--- src/utilities/ad.jl
+++ src/utilities/ad.jl
@@ -1,8 +1,12 @@
 using Zygote: pullback
 using ProximalCore
 
-function ProximalCore.gradient!(grad, f, x)
-    fx, pb = pullback(f, x)
+struct ZygoteFunction{F}
+    f::F
+end
+
+function ProximalCore.gradient!(grad, f::ZygoteFunction, x)
+    fx, pb = pullback(f.f, x)
     grad .= pb(one(fx))[1]
     return fx
 end
