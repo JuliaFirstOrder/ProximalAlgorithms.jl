2b6f8e96885a780ee72b494fe822c7d59e815f57
Lorenzo Stella <lorenzostella@gmail.com>
2023-09-22 00:08:28 +0200

    Update ad.jl

--- src/utilities/ad.jl
+++ src/utilities/ad.jl
@@ -5,6 +5,8 @@ struct ZygoteFunction{F}
     f::F
 end
 
+(f::ZygoteFunction)(x) = f.f(x)
+
 function ProximalCore.gradient!(grad, f::ZygoteFunction, x)
     fx, pb = pullback(f.f, x)
     grad .= pb(one(fx))[1]
